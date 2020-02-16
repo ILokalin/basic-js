e2835589eb14bbe266fe1de6bdc3fee7f4d203a4
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-16 19:42:24 +0500

    feat(countCats): create function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,6 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(room) {
+  
+  return room.reduce((summ, current, index, array) => {
+    return summ + array[index].filter(item => item === '^^').length;
+  }, 0);
 };
