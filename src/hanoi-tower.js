f0782ae57dc6bc4d400072b5e0203e037b641590
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-17 05:47:17 +0500

    refactor(hanoi-tower): change formatting in params and update missing semi-colon

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,8 +1,8 @@
-module.exports = function calculateHanoi(diskNumber,turnsSpeed) {
+module.exports = function calculateHanoi(diskNumber, turnsSpeed) {
     let turns = Math.pow(2, diskNumber) - 1; 
 
     return {
         turns,
         seconds: turns / (turnsSpeed / 3600)
-    }
+    };
 }
