92db01ea29a2510f4e2eeef81129c60bff60924c
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-17 07:41:10 +0500

    feat: create function for without 'action flags work properly'

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,24 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(workArray) {
+    const result = [];
+
+    if (!Array.isArray(workArray)) {
+        throw 'Error';
+    }
+    
+    workArray.forEach((item, index, array) => {
+            if (workArray[index + 1] !== '--discard-prev' && !/^--/.test(item) && workArray[index - 1] !== '--discard-next') {
+                result.push(item);
+            } 
+
+            if (item === '--double-next' && !/^--/.test(workArray[index + 1]) && index < workArray.length - 1) {
+                result.push(workArray[index + 1]);
+            }
+
+            if (item === '--double-prev' && !/^--/.test(workArray[index - 1]) && index > 0) {
+                result.push(workArray[index - 1]);
+            }
+
+    });
+
+    return result;
 };
