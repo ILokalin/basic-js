c7c68281664c11cadf577f6abc8f647f4eee9a75
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-18 04:16:35 +0500

    feat(simple-chain): create solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,42 @@
 const chainMaker = {
+  chain: [],
+  countChain: 0,
+
+  clearChain() {
+    this.countChain = 0;
+    this.chain = [];
+  },
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+
+    return this.countChain;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(value + '');
+    this.countChain++;
+
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position > 0 && position <= this.countChain && typeof position === 'number') {
+      this.chain.splice(position - 1, 1);
+      this.countChain--;
+ 
+      return this;
+    } else {
+      this.clearChain();
+      throw 'Error';
+    }
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = `( ${this.chain.join(' )~~( ')} )`;
+    this.clearChain();
+
+    return result;
   }
 };
 
