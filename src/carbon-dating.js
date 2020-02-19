dde72b539d2e48210cbf605974e2af8eb28ad6bc
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-19 06:02:09 +0500

    refactor: correct converter and conditional

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,9 +3,9 @@ const HALF_LIFE_PERIOD= 5730;
 const LOGARITHM_NATURAL_FROM_2 = 0.693;
 
 module.exports = function dateSample(stamp) {
-  let stampNumber = +/-?\d+\.?\d*/.exec(stamp);
+  let stampNumber = Number(/-?\d+\.?\d*/.exec(stamp));
 
-  if (typeof +stamp !== 'undefined' && typeof stamp === 'string' && stampNumber > 0 && stampNumber < MODERN_ACTIVITY) {
+  if (typeof stampNumber !== 'NaN' && typeof stamp === 'string' && stampNumber > 0 && stampNumber < MODERN_ACTIVITY) {
 
     return Math.ceil(HALF_LIFE_PERIOD / (LOGARITHM_NATURAL_FROM_2 / Math.log(MODERN_ACTIVITY / stampNumber) ));
   }
