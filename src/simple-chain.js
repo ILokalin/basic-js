460275231b9fec8fd6ad9a1103b5c82274a73652
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-19 07:50:16 +0500

    refactor: create template for result, optimize code

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,42 +1,41 @@
 const chainMaker = {
+  CHAIN_SEPARATOR:  ' )~~( ',
+  CHAIN_RESULT_TEMPLATE: "`( ${result} )`",
+  
   chain: [],
-  countChain: 0,
 
-  clearChain() {
-    this.countChain = 0;
-    this.chain = [];
-  },
   getLength() {
-
-    return this.countChain;
+    return this.chain.length;
   },
+
   addLink(value) {
     this.chain.push(value + '');
-    this.countChain++;
 
     return this;
   },
+
   removeLink(position) {
-    if (position > 0 && position <= this.countChain && typeof position === 'number') {
+    if (position > 0 && position <= this.chain.length && typeof position === 'number') {
       this.chain.splice(position - 1, 1);
-      this.countChain--;
- 
+  
       return this;
     } else {
-      this.clearChain();
+      this.chain = [];
       throw 'Error';
     }
   },
+
   reverseChain() {
     this.chain.reverse();
 
     return this;
   },
-  finishChain() {
-    let result = `( ${this.chain.join(' )~~( ')} )`;
-    this.clearChain();
 
-    return result;
+  finishChain() {
+    let result = this.chain.join(this.CHAIN_SEPARATOR);
+    this.chain = [];
+
+    return eval(this.CHAIN_RESULT_TEMPLATE);
   }
 };
 
