c6f43d0f9402d4924965b7938422a76774c52af2
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-19 06:40:04 +0500

    refactor: create default constant for seconds

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,8 +1,10 @@
+const SECONDS_PER_HOUR = 3600;
+
 module.exports = function calculateHanoi(diskNumber, turnsSpeed) {
     let turns = Math.pow(2, diskNumber) - 1; 
 
     return {
         turns,
-        seconds: turns / (turnsSpeed / 3600)
+        seconds: turns / (turnsSpeed / SECONDS_PER_HOUR)
     };
 }
