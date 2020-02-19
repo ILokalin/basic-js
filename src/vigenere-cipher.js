6b83103885e7b8776be8d3d5baa6fb7112f43ae3
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-19 05:01:54 +0500

    feat(encrypt): create base encrypt function with direct mode

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,49 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor (directCoder = true) {
+        this.direct = directCoder
+
+        this.codeFromChar = this.codeFromChar.bind(this);
+        this.charFromCode = this.charFromCode.bind(this);
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    charFromCode(code) {
+        return String.fromCharCode(code + 65);
+    }
+
+    codeFromChar(char) {
+        return char.toUpperCase().charCodeAt(char) - 65;
+    }
+
+    encrypt(string = '', key = '') {
+        let encryptString = '',
+            keyCount = 0;
+
+        if (string === '' || key === '') {
+            throw 'Error';
+        }
+       
+        for (let i = 0, length = string.length; i < length; i++) {
+            let cryptChar = string[i];
+
+            if (/[A-Za-z]/.test(cryptChar)) {
+                encryptString += this.charFromCode((this.codeFromChar(cryptChar) +  this.codeFromChar(key[keyCount++ % key.length])) % 26);
+            } else {
+                encryptString += cryptChar;
+            }
+            
+        }
+        console.log(encryptString)
+        return encryptString;
+        
+    }
+
+    decrypt(string = '', key = '') {
+        
+
+            if (string === '' || key === '') {
+            throw 'Error';
+        }
+
     }
 }
 
