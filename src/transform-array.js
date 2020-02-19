9083c3afa4354ac9f39fcea240f34a21dfa301ed
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-19 07:59:41 +0500

    refactor: optimize code and create tempaltes for commands

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,25 +1,31 @@
-module.exports = function transform(workArray) {
-    
-    const result = [];
+const COMMAND_DISCARD_NEXT = '--discard-next',
+      COMMAND_DISCARD_PREV = '--discard-prev',
+      COMMAND_DUBLE_NEXT = '--double-next',
+      COMMAND_DUBLE_PREV = '--double-prev',
+      COMMAND_REGEXP = /^--/;
 
+
+module.exports = function transform(workArray) {
     if (!Array.isArray(workArray)) {
         throw 'Error';
     }
-    
-    workArray.forEach((item, index, array) => {
-            if (!/^--/.test(item) && workArray[index - 1] !== '--discard-next') {
+
+    const result = [];
+
+    workArray.forEach((item, index) => {
+            if (!COMMAND_REGEXP.test(item) && workArray[ index - 1 ] !== COMMAND_DISCARD_NEXT ) {
                 result.push(item);
             } 
 
-            if (item === '--double-next' && !/^--/.test(workArray[index + 1]) && index < workArray.length - 1) {
+            if (item === COMMAND_DUBLE_NEXT && !COMMAND_REGEXP.test(workArray[ index + 1 ]) && index < workArray.length - 1) {
                 result.push(workArray[index + 1]);
             }
 
-            if (item === '--double-prev' && !/^--/.test(workArray[index - 1]) && index > 0) {
+            if (item === COMMAND_DUBLE_PREV && !COMMAND_REGEXP.test(workArray[ index - 1 ]) && index > 0) {
                 result.push(workArray[index - 1]);
             }
 
-            if (item === '--discard-prev') {
+            if (item === COMMAND_DISCARD_PREV) {
                 result.pop();
             }
     });
